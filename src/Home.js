8835f4ddc8490877f11430cd8d14a87c3d522d9a
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2022-12-30 19:41:41 -0500

    basic outline of projects

--- src/Home.js
+++ src/Home.js
@@ -21,29 +21,8 @@ export default function Home() {
             <div className="text-2xl space-y-3">
                 <p>I'm a senior at <a className="text-purple-400 border-white inline-block rounded hover:bg-purple-300 hover:text-black" href="https://www.bergen.org/bca">Bergen County Academies </a> studying in ATCS (Academy of Technology and Computer Science).</p><p> I like making games + experimenting with web development and machine learning in my free time. </p>
                 <br></br>
-               
+                <br></br>
             </div>
-
-            {/* <MDBAccordion flush initialActive={1}>
-                <MDBAccordionItem collapseId={1} headerTitle='Education + Courses'>
-                    <ol>Bergen County Academies 2019-2023
-                        <li>Intro to Computer Science</li>
-                        <li></li>
-                    </ol>
-                </MDBAccordionItem>
-                <MDBAccordionItem collapseId={2} headerTitle='Accordion Item #2'>
-                </MDBAccordionItem>
-                <MDBAccordionItem collapseId={3} headerTitle='Accordion Item #3'>
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                    moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                    Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                    proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                    aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </MDBAccordionItem>
-            </MDBAccordion>
-            ); */}
-
 
             <Carousel fade>
                 <Carousel.Item>
@@ -54,7 +33,7 @@ export default function Home() {
                     <Carousel.Caption>
                         <h3 className="text-white border-black inline-block rounded bg-black py-1 px-1">Hi!</h3>
                         <br></br>
-                        <p  className="text-white border-black inline-block rounded font-normal text-size-2xl bg-black py-1 px-1">I love to go to the beach in the Summer.</p>
+                        <p className="text-white border-black inline-block rounded font-normal text-size-2xl bg-black py-1 px-1">I love to go to the beach in the Summer.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -84,11 +63,12 @@ export default function Home() {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-
+            <br></br>
+            <br></br>
             <div className="edu">
                 <Accordion defaultActiveKey="0" flush>
                     <Card>
-                        <Accordion.Header><Card.Header>Contact Me</Card.Header></Accordion.Header>
+                        <Accordion.Header><Card.Header>Contact Me:</Card.Header></Accordion.Header>
                         <Accordion.Body>
                             <Card.Body>
                                 <Card.Text>
--- src/Project.js
+++ src/Project.js
@@ -1,22 +1,123 @@
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
 export default function Project() {
-  return (
-    <h1>hello</h1>
-    /* <div className="edu">
-                <Accordion defaultActiveKey="0" flush>
-                    <Card>
-                        <Accordion.Header><Card.Header>Education</Card.Header></Accordion.Header>
-                        <Accordion.Body>
-                            <Card.Body>
-                                <Card.Title>BCA 2019-2023</Card.Title>
-                                <Card.Text>
-                                    ATCS Academy of Technology and Computer Science
-                                    UDemy Course
-                                </Card.Text>
-                                {/* <Button variant="primary">Go somewhere</Button> */
-    /* </Card.Body>
-                            </Accordion.Body>
-                        </Card>
-                    </Accordion>
-                </div>  */
-  )
+    return (
+        <div className="container pl-20 pr-20">
+            <br></br>
+            <br></br>
+            <h1 className="text-5xl">Projects:</h1>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Current Project: Alumni Portal</Card.Title>
+                    <Card.Text>
+                        Prototype Demo for clients to give feedback on the portal's functions.
+                        We are currently working with administration to create the first ever BCA-Alumni Portal.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="/https://github.com/RemingtonKim/AcademiesAlumni">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>AP Microeconomics Dating Simulation</Card.Title>
+                    <Card.Text>
+                        Was created for a unit review presentation in microeconomics. 
+                        I wanted to engage my classmates in something fun at the end of the lesson, while reviewing
+                        the material. 5 questions where the player tries to win the textbook's heart.
+
+                        There are a total of 6 endings and which ending is played depends on the player's correct answers.
+
+                        The pictures are of BCA.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/MicroDatingSimulation">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Natto Obstacle Course</Card.Title>
+                    <Card.Text>
+                        aksfjkl;sdjf;sdjfksd;fj
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/Natto_Obstacle_Course">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Music Classifier</Card.Title>
+                    <Card.Text>
+                        ksafjl;kasfjklasjflks;df
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/MusicClassifier">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>BlackJack GUI</Card.Title>
+                    <Card.Text>
+                        akljfkl;sjfkl;asdjfsdf
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/Midyear-Project-Blackjack">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Rocket Game</Card.Title>
+                    <Card.Text>
+                        sadfjlkasdfj;lksdjfksdf
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/rocket">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Experimental: Music / CSS Website</Card.Title>
+                    <Card.Text>
+                        alskjf;lsadf
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="https://github.com/Yun505/MakeAWebPageProject">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+            <br></br>
+            <Card style={{ width: '450' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body>
+                    <Card.Title>Librosa Model</Card.Title>
+                    <Card.Text>
+                      asdfkljsdklfj;sf
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Card.Link href="/https://github.com/RemingtonKim/AcademiesAlumni">GitHub</Card.Link>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
