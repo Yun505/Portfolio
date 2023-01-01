6eef8a405d1c4da2dbf9abe06d80def66327387c
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2023-01-01 04:03:24 -0500

    UI of projects done

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,6 @@
-import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-
+import "./dist/output.css"
+import "./App.css"
 export default function Project() {
     return (
         <div className="container pl-20 pr-20">
@@ -8,9 +8,9 @@ export default function Project() {
             <br></br>
             <h1 className="text-5xl">Projects</h1>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <Card.Body>
+            <Card  className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }} >
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/ijfOmpE0Msg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <Card.Body className="text-medium">
                     <Card.Title>Current Project: Alumni Portal</Card.Title>
                     <Card.Text>
                         Prototype Demo for clients to give feedback on the portal's functions.
@@ -18,25 +18,25 @@ export default function Project() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="/https://github.com/RemingtonKim/AcademiesAlumni">GitHub</Card.Link>
+                    <Card.Link className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2" href="/https://github.com/RemingtonKim/AcademiesAlumni">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <Card  className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/Ot85pQ76atM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>AP Microeconomics Dating Simulation</Card.Title>
                     <Card.Text>
-                        
+
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/MicroDatingSimulation">GitHub</Card.Link>
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://github.com/Yun505/MicroDatingSimulation">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <Card  className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }}>
+                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/1jkjqUn7ncQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>Natto Obstacle Course</Card.Title>
                     <Card.Text>
@@ -44,25 +44,25 @@ export default function Project() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/Natto_Obstacle_Course">GitHub</Card.Link>
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://github.com/Yun505/Natto_Obstacle_Course">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <Card.Body>
+            <Card  className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }}>
+            <iframe width="stretch" height="315" src="https://www.youtube.com/embed/GSpHw8jqjdk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+               <Card.Body>
                     <Card.Title>Music Classifier</Card.Title>
                     <Card.Text>
                         ksafjl;kasfjklasjflks;df
                     </Card.Text>
                 </Card.Body>
-                <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/MusicClassifier">GitHub</Card.Link>
+                <Card.Body  >
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://github.com/Yun505/MusicClassifier">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <Card className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }}>
+            <iframe width="stretch" height="315" src="https://www.youtube.com/embed/tuTCCPOBf8E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>BlackJack GUI</Card.Title>
                     <Card.Text>
@@ -70,12 +70,12 @@ export default function Project() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/Midyear-Project-Blackjack">GitHub</Card.Link>
+                    <Card.Link className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2" href="https://github.com/Yun505/Midyear-Project-Blackjack">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
-                <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <Card className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }}>
+            <iframe width="stretch" height="315" src="https://www.youtube.com/embed/3Bsgfa6kNRA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>Rocket Game</Card.Title>
                     <Card.Text>
@@ -83,11 +83,11 @@ export default function Project() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/rocket">GitHub</Card.Link>
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://github.com/Yun505/rocket">GitHub</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
+            <Card  className="hover:shadow-lg"style={{ width: '450',  backgroundColor: '#e9d5ff' }} >
                 <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>Experimental: Music / CSS Website</Card.Title>
@@ -96,11 +96,12 @@ export default function Project() {
                     </Card.Text>
                 </Card.Body>
                 <Card.Body>
-                    <Card.Link href="https://github.com/Yun505/MakeAWebPageProject">GitHub</Card.Link>
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://github.com/Yun505/MakeAWebPageProject">GitHub</Card.Link>
+                    <Card.Link  className="text-purple-800 border-white inline-block rounded hover:bg-purple-300 hover:text-black px-2"href="https://yun505.github.io/MakeAWebPageProject/">Website</Card.Link>
                 </Card.Body>
             </Card>
             <br></br>
-            <Card style={{ width: '450' }}>
+            {/* <Card style={{ width: '450' }}>
                 <iframe width="stretch" height="315" src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Card.Body>
                     <Card.Title>Librosa Model</Card.Title>
@@ -111,7 +112,7 @@ export default function Project() {
                 <Card.Body>
                     <Card.Link href="/https://github.com/RemingtonKim/AcademiesAlumni">GitHub</Card.Link>
                 </Card.Body>
-            </Card>
+            </Card> */}
         </div>
     );
 }
